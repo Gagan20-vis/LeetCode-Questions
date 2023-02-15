2a9b20d8ee6ad69518d5858cceecdf0bbb044d26
Gagan Vishwakarma <gaganvishwakarma46@gmail.com>
2023-02-15 16:17:40 +0530

    Merge Sorted Array

---
+++  Merge Sorted Array/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //Brute force
+
+        // int j = 0;
+        // for(int i=m;i<m+n;i++)
+        //     nums1[i] = nums2[j++];
+        // Arrays.sort(nums1);
+
+        //Optimal
+        int i = m-1;
+        int j = n-1;
+        int k= nums1.length-1;
+
+        while(j>=0)
+        {
+            if(i>=0 && nums1[i]> nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+    }
+
+}
