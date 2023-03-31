a14931cd44b187e810b944a549d27dc3e89064a2
Gagan <gaganvishwakarma46@gmail.com>
2023-03-31 20:46:30 +0530

    Increasing Triplet Subsequence

---
+++ Increasing Triplet Subsequence/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean increasingTriplet(int[] arr) {
+        int num1 = Integer.MAX_VALUE;
+        int num2 = Integer.MAX_VALUE;
+        for(int num: arr){
+            if(num<=num1) num1 = num;
+            else if (num<=num2) num2 = num;
+            else return true;
+        }
+        return false;
+    }
+}
