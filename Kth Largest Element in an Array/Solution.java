d37dd15386bc081818a4e1b1a9837a38c21d8b1e
Gagan <gaganvishwakarma46@gmail.com>
2023-05-08 09:31:18 +0530

    Kth Largest Element in an Array

---
+++ Kth Largest Element in an Array/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+
+    int quickSelect(int[] nums, int low, int high, int k) {
+        int pivot = low;
+        for (int j = low; j < high; j++)
+            if (nums[j] <= nums[high])
+                swap(nums, pivot++, j);
+        swap(nums, pivot, high);
+        int count = high - pivot + 1;
+        if (count == k) return nums[pivot];
+        if (count > k) return quickSelect(nums, pivot + 1, high, k);
+        return quickSelect(nums, low, pivot - 1, k - count);
+    }
+    void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
